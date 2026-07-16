d7b84a21eba428f407008bc390d690f2c9fec90c
Pasizhang <zhang.yuxuan1510@gmail.com>
2026-07-16 12:47:27 +0300

    Add new item from

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    place TEXT,
+    times TEXT,
+    players INTEGER,
+    user_id INTEGER REFERENCES users
+);
