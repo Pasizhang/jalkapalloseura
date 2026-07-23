4d28e815093c8738ab03dcef64d3ca450c52abed
Pasizhang <zhang.yuxuan1510@gmail.com>
2026-07-23 21:43:58 +0300

    Add find item feature

--- items.py
+++ items.py
@@ -30,3 +30,11 @@ def update_item(item_id, place, times, players):
 def remove_item(item_id):
     sql = "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
+
+def find_items(query):
+    sql = """SELECT id, place
+             FROM items
+             WHERE place LIKE ? or times LIKE ?
+             ORDER BY id DESC"""
+    like = "%" + query + "%"
+    return db.query(sql, [like, like])
